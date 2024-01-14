4b7c5b43570569007abb8a680eaa42c0d9137464
DeveloperOybek <programmeroybek211@gmail.com>
2024-01-14 17:46:19 +0500

    fdfad

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom"
 import Menu from "../menu/menu"
 import About from "../about/about"
-import Comments from "../comments/comments"
-import Navbar from "../navbar/navbar"
 import './app.css'
+import Navbar from "../navbar/navbar"
+import Comments from "../comments/comments"
+import Foods from "../foods/foods"
 const App = () => {
     return (
         <div className="app container ">
@@ -13,6 +14,7 @@ const App = () => {
                     <Route path="/" element={<Menu />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Comments />} />
+                    <Route path="/foods" element={<Foods />} />
                 </Routes>
             </div>
         </div>
