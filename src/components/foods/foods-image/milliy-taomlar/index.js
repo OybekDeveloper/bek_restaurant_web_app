5e7c1fab960a9e4cd87083f2d1fcf2bf81e4d58c
DeveloperOybek <programmeroybek211@gmail.com>
2024-01-15 21:51:16 +0500

    finish him

--- src/components/foods/foods-image/milliy-taomlar/index.js
+++ src/components/foods/foods-image/milliy-taomlar/index.js
@@ -2,7 +2,7 @@ const MilliyTaomlar = {
   chaxoxbili: new URL("./chaxoxbili.jpg", import.meta.url).href,
   dolma: new URL("./dolma.jpg", import.meta.url).href,
   jizsamarqand: new URL("./jiz samarqand.jpg", import.meta.url).href,
-  jizumakay: new URL("./jizumakay.jpg", import.meta.url).href,
+  jizumakay: new URL("./jiz umakay.jpg", import.meta.url).href,
   qozichoqq: new URL("./qo'zichoq qovurg'asi.jpg", import.meta.url).href,
   qozichoqy: new URL("./qo'zichoq yelkasi.jpg", import.meta.url).href,
   qozonkabob: new URL("./qozon kabob.jpg", import.meta.url).href,
