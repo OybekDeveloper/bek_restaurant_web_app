4ecfc0dadb46713bd22f37938f5777bee8ac37c8
DeveloperOybek <programmeroybek211@gmail.com>
2024-01-17 13:58:22 +0500

    finish

--- src/components/foods/foods-img.jsx
+++ src/components/foods/foods-img.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Blurhash } from 'react-blurhash';
 import './foods.css';
-
+//eslint-disable-next-line
 export const FoodsImage = ({ src }) => {
     const [imgLoading, setImageLoading] = useState(false);
     const baseUrl = 'https://media.istockphoto.com/id/1367855191/fr/vectoriel/galerie-dimages-ic%C3%B4ne-solide.jpg?s=612x612&w=0&k=20&c=6YcYJhK-H6i2wto10SJvSa-Y06TvzpM6aVOvBgUSWdo=';
@@ -16,14 +16,16 @@ export const FoodsImage = ({ src }) => {
 
     return (
         <div>
-            <div>
+            <div  >
                 <Blurhash
                     style=
                     {{
-                        display: imgLoading ? 'none' : 'block',
-                        height: '150px', width: '100vw',
-                        borderRadius: '20px', objectFit: 'cover'
+                        display:imgLoading ? 'none' : 'block',
+                        height: '150px', width: '100%',
+                        objectFit: 'cover'
                     }}
+                className='imgMenu rounded-[20px]'
+
                     hash='LEHV6nWB2yk8pyo0adR*.7kCMdnj'
                     width={200}
                     height={200}
